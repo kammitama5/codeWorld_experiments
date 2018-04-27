437319d09f401e238f63aff9bf18f0e695f70e7e
kammitama5 <krystal.maughan@gmail.com>
2018-04-27 13:07:49 -0700

    notes for chapter2

---
+++ play_img/notes2.hs
@@ -0,0 +1,22 @@
+-- going back and forth between chapters to remember functions
+-- eg. I couldn't remember translucent so had to go back to chpt 1 
+-- even though I was on chpt 1
+
+-- I couldn't tab :/ it would shift entire line
+-- eg. tab = to push = forward would tab entire line of code 
+
+-- is there any way to make the code part smaller or larger 
+-- like a drag to increase the ratio of editor vs viewer? eg like repl.it
+
+program = drawingOf(mouse)
+mouse = colored(ear1, translucent(black)) &
+  colored(ear2, translucent(black)) &
+  colored(body, (translucent(black)))
+  where ear1 = translated(solidCircle(1), 2, 2)
+        ear2 = translated(solidCircle(1), -2, 2)
+		body = solidCircle(2)
+		
+-- pattern matching 
+-- f(x) | x == pi = 1
+--      | otherwise = 2
+
